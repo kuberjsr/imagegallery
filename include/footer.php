bf25075a2c9983b9fb24f82e895fd3caa24aa39b
Raman Aggarwal <kuberjsr@gmail.com>
2015-03-25 12:45:34 +0530

    Create footer.php

---
+++ include/footer.php
@@ -0,0 +1,4 @@
+		<script type="text/javascript" src="gallery/js/jquery.tmpl.min.js"></script>
+		<script type="text/javascript" src="gallery/js/jquery.easing.1.3.js"></script>
+		<script type="text/javascript" src="gallery/js/jquery.elastislide.js"></script>
+		<script type="text/javascript" src="gallery/js/gallery.js"></script>
