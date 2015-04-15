d2bde78b352283113f13e60e78b0f53d657841b4
Raman Aggarwal <kuberjsr@gmail.com>
2015-04-15 12:43:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>Image gallery</title>
 <?php include("gallery/include/upper.php") ?>
+<script type="text/javascript" src="js/jquery.min.js"></script>
 </head>
 
 <body>
